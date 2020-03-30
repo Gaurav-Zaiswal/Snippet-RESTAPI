57e702feedf51a0d8e67509a1da3bdce19a356ec
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-03-30 18:20:27 +0545

    class based views are implemented

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
